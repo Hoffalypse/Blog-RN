39a0006e7ad7901654a8b52684810426fff69a70
Bryan W <bhoff1980@gmail.com>
2023-03-20 05:15:54 -0600

    figured out render issue and begin work on show individual post screen

--- src/screens/IndexScreen.js
+++ src/screens/IndexScreen.js
@@ -1,23 +1,37 @@
 import React, { useContext } from 'react';
-import { View, Text, StyleSheet, FlatList, Button } from 'react-native';
+import {
+  View,
+  Text,
+  StyleSheet,
+  FlatList,
+  Button,
+  TouchableOpacity
+} from 'react-native';
 import { Context } from '../context/BlogContext';
-import { Feather } from '@expo/vector-icons'
+import { Feather } from '@expo/vector-icons';
 
-const IndexScreen = () => {
-  const { state, addBlogPost } = useContext(Context);
+const IndexScreen = ({navigation}) => {
+  const { state, addBlogPost, deleteBlogPost } = useContext(Context);
 
   return (
     <View>
-      
       <Button title="Add Post" onPress={addBlogPost} />
       <FlatList
         data={state}
         keyExtractor={blogPost => blogPost.title}
         renderItem={({ item }) => {
-          return <View style={styles.row}>
-            <Text style={styles.title}>{item.title}</Text>
-            <Feather name='trash-2' size={24}/>
+          return (
+            <TouchableOpacity onPress={() => navigation.navigate('Show', {id:item.id})}>
+            <View style={styles.row}>
+              <Text style={styles.title}>
+                {item.title} - {item.id}
+              </Text>
+              <TouchableOpacity onPress={() => deleteBlogPost(item.id)}>
+                <Feather style={styles.icon} name="trash-2" />
+              </TouchableOpacity>
             </View>
+            </TouchableOpacity>
+          );
         }}
       />
     </View>
@@ -26,16 +40,18 @@ const IndexScreen = () => {
 
 const styles = StyleSheet.create({
   row: {
-    flexDirection:'row',
-    justifyContent:'space-between',
-    paddingVertical:20,
-    paddingHorizontal:20,
-    borderTopWidth:1,
-    
-    borderColor: 'black'
+    flexDirection: 'row',
+    justifyContent: 'space-between',
+    paddingVertical: 20,
+    paddingHorizontal: 25,
+    borderTopWidth: 1,
+    borderColor: 'gray'
   },
   title: {
-    fontSize:18,
+    fontSize: 18
+  },
+  icon: {
+    fontSize: 24
   }
 });
 
