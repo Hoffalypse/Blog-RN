561d3ee01cf01c50c4c289ac01a45705302929eb
Bryan W <bhoff1980@gmail.com>
2023-03-19 20:10:32 -0600

    using state and context but going to refactor to useReducer to make better code

--- src/IndexScreen.js
+++ src/IndexScreen.js
@@ -1,10 +1,24 @@
-import { View, Text } from 'react-native'
-import React from 'react'
+import { View, Text, FlatList, Button} from 'react-native'
+import React, { useContext } from 'react'
+import BlogContext from '../context/BlogContext'
+
 
 const IndexScreen = () => {
+
+    const {data, addBlogPost} = useContext(BlogContext)
   return (
     <View>
       <Text>Index Screen</Text>
+      <Button 
+      title='Add Post'
+      onPress={addBlogPost}
+      />
+      <FlatList 
+      data={data}
+      keyExtractor={(blogPost) => blogPost.title}
+      renderItem={({item}) => <Text>{item.title}</Text>}
+      />
+
     </View>
   )
 }
